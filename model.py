0100a84a8740feffd50cdfbaa6a9ed6096be610b
Jiwon Hwang <hjw5167@gmail.com>
2018-08-07 13:50:11 +0900

    change network width

--- model.py
+++ model.py
@@ -8,9 +8,9 @@ class SRCNN(nn.Module):
         super(SRCNN, self).__init__()
 
         self.layers = nn.Sequential(
-            ConvBlock(3, 64,  kernel_size=9, padding=4),
-            ConvBlock(64, 32, kernel_size=1, padding=0),
-            ConvBlock(32, 3,  kernel_size=5, padding=2, activation=None))
+            ConvBlock(3, 256,  kernel_size=9, padding=4),
+            ConvBlock(256, 128, kernel_size=1, padding=0),
+            ConvBlock(128, 3,  kernel_size=5, padding=2, activation=None))
     
     def forward(self, x):
         return self.layers(x)
