60001caa943f076761c57a0fcf756b9fadef989f
jiwon hwang <hjw5167@gmail.com>
2018-08-12 14:31:36 +0900

    change the model, increasing PSNR

--- my_model.py
+++ my_model.py
@@ -14,14 +14,11 @@ class SRCNN(nn.Module):
             ConvBlock(32, 3,  kernel_size=5, padding=2, activation=None))
         '''
         self.layers = nn.Sequential(
-            ConvBlock(3, 64, kernel_size=3, padding=1),
-            ResnetBlock(64),
-            ResnetBlock(64),
+            ConvBlock(3, 64, kernel_size=9, padding=4),
 	    ResnetBlock(64),
-            ResnetBlock(64),
-	    ResnetBlock(64),
-	    ResnetBlock(64),
-            ConvBlock(64, 3, kernel_size=3, padding=1, activation=None))
+            ConvBlock(64, 32, kernel_size=1, padding=0),
+            ResnetBlock(32),
+            ConvBlock(32, 3, kernel_size=5, padding=2, activation=None))
     
     def forward(self, x):
         return (self.layers(x) + x)
